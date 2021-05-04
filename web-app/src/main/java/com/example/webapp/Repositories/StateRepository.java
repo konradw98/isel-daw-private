691be5693fb91b9d434f3d93fed89ff6646fd4d2
konrad <konradwodnicki98@gmail.com>
2021-05-04 09:00:33 +0100

    add httpp basic security

--- web-app/src/main/java/com/example/webapp/Repositories/StateRepository.java
+++ web-app/src/main/java/com/example/webapp/Repositories/StateRepository.java
@@ -1,2 +1,8 @@
-package com.example.webapp.Repositories;public interface StateRepository {
+package com.example.webapp.Repositories;
+
+import com.example.webapp.Models.Project;
+import com.example.webapp.Models.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State,Long> {
 }
